836b2be06d1aa1a8b3f23ab3033058c22c3d90ca
yotubarail <47407052+yotubarail@users.noreply.github.com>
2020-03-25 17:28:49 +0900

    Update sitirigahamaViewController.swift

--- enodenhome/enodenStations/sitirigahamaViewController.swift
+++ enodenhome/enodenStations/sitirigahamaViewController.swift
@@ -25,7 +25,7 @@ class sitirigahamaViewController: UIViewController, UIImagePickerControllerDeleg
         imagePick.delegate = self
 
         let storage = Storage.storage()
-        let reference = storage.reference(forURL: "gs://enodenhome.appspot.com")
+        let reference = storage.reference(forURL: "gs://XXXXX.appspot.com")
         let child = reference.child("SitirigahamaImages/" + user!.uid + "/"+"sitirigahama.jpg")
         child.getData(maxSize: 1 * 1024 * 1024) { data, error in
             if error != nil {
@@ -84,7 +84,7 @@ class sitirigahamaViewController: UIViewController, UIImagePickerControllerDeleg
     // Firebaseにアップロード
     fileprivate func upload() {
      
-     let storageRef = Storage.storage().reference(forURL: "gs://enodenhome.appspot.com").child("SitirigahamaImages/" + user!.uid + "/"+"sitirigahama.jpg")
+     let storageRef = Storage.storage().reference(forURL: "gs://XXXXX.appspot.com").child("SitirigahamaImages/" + user!.uid + "/"+"sitirigahama.jpg")
         let metaData = StorageMetadata()
         metaData.contentType = "image/jpg"
         if let uploadData = self.sitirigahamaImage.image?.jpegData(compressionQuality: 0.3) {
