160347e7ebb6192ce3ba7261df6ba27177fb8c07
yotubarail <47407052+yotubarail@users.noreply.github.com>
2020-04-14 11:29:58 +0900

    Update haseViewController.swift

--- enodenhome/enodenStations/haseViewController.swift
+++ enodenhome/enodenStations/haseViewController.swift
@@ -25,7 +25,7 @@ class haseViewController: UIViewController, UIImagePickerControllerDelegate,UINa
         imagePick.delegate = self
         
         let storage = Storage.storage()
-        let reference = storage.reference(forURL: "gs://enodenhome.appspot.com")
+        let reference = storage.reference(forURL: "gs://XXX.appspot.com")
         let child = reference.child("HaseImages/" + user!.uid + "/"+"hase.jpg")
 
         haseImage.sd_setImage(with: child)
